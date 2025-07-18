47ef97fbde5dc7d43409df40ceb1e632e27b32c8
DragonFruit8 <Jtbyers19@gmail.com>
2025-07-18 08:45:13 -0400

    feat: Stats meter added

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,8 +1,10 @@
+// import FeatureStats from './FeatureStats';
 // const current =
 //   'https://www.gofundme.com/f/support-our-struggle-for-food-water-survival/widget/large?sharesheet=undefined&attribution_id=sl:1d6695db-d20a-4828-b77e-e1946ea248bc';
 
 function Featured () {
   return (
+    <>
     <div className="container">
     <div className="col-md-4">
     </div>
@@ -12,6 +14,8 @@ function Featured () {
       ></iframe>
       <script src="https://www.gofundme.com/static/js/embed.js" />
     </div>
+    {/* <FeatureStats /> */}
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar () {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">Collective Network</Link>
         <button
