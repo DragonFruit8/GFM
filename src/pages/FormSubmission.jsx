4003e0a71bc0b2e0ceee7929c6ed6718e530a0ba
DragonFruit8 <Jtbyers19@gmail.com>
2025-07-22 14:49:03 -0400

    feat: iframe loading: eager

--- src/pages/FormSubmission.jsx
+++ src/pages/FormSubmission.jsx
@@ -1,18 +1,17 @@
-
-
-function FormSubmission () {
-  return (
-    <div>
-      <iframe
-        className="airtable-embed airtable-form"
-        src="https://airtable.com/embed/app2EnT4mFuanmfyF/pagsu3F1L3N1XtZnw/form"
-        frameborder="0"
-        // onmousewheel=""
-        width="100%"
-        height="800px"
-      />
-    </div>
-  );
+function FormSubmission() {
+    return (
+        <div>
+            <iframe
+                className="airtable-embed"
+                src="https://airtable.com/embed/app2EnT4mFuanmfyF/pagsu3F1L3N1XtZnw/form"
+                frameborder="0"
+                onmousewheel=""
+                width="100%"
+                height="800"
+                loading="eager"
+            ></iframe>
+        </div>
+    )
 }
 
-export default FormSubmission;
+export default FormSubmission
