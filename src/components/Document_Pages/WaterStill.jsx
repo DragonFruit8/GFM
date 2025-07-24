a22b08444ec7b3f3c2275d71d9b33dddd32f6c1a
DragonFruit8 <Jtbyers19@gmail.com>
2025-07-23 20:19:15 -0400

    feat: Sidebar - Link modification

--- src/components/Document_Pages/WaterStill.jsx
+++ src/components/Document_Pages/WaterStill.jsx
@@ -17,23 +17,23 @@ function WaterStill () {
             <p className="lead fs-4"><strong>DIY Solar Still</strong></p>
             <ol>
               <li>
-                <strong>Plastic sheeting (clear or black):</strong>
+                <strong>Plastic sheeting (clear or black):  </strong>
                 This could be obtained from plastic bags, tarps, or other plastic debris.
               </li>
               <li>
-                <strong>A container for collecting water:</strong>
+                <strong>A container for collecting water:  </strong>
                 This could be a plastic bottle, a metal can, or any other container that can hold water.
               </li>
               <li>
-                <strong>A tube or straw:</strong>
+                <strong>A tube or straw:  </strong>
                 This could be made from plastic tubing or a length of flexible plastic, such as a water hose.
               </li>
               <li>
-                <strong>Rocks or sand:</strong>
+                <strong>Rocks or sand:  </strong>
                 These could be used to anchor the plastic sheeting and create a sloped surface for condensation to collect.
               </li>
               <li>
-                <strong>A clean cloth or sponge:</strong>
+                <strong>A clean cloth or sponge:  </strong>
                 This could be used to absorb the condensation and transfer it to the container.
               </li>
             </ol>
@@ -59,23 +59,23 @@ function WaterStill () {
               </ul> */}
             <ol>
               <li>
-                <strong>Plastic sheeting:</strong>
+                <strong>Plastic sheeting:  </strong>
                 Approximately 4-6 square meters of clear plastic sheeting, or enough to cover the size of the still.
               </li>
               <li>
-                <strong>Collection container:</strong>
+                <strong>Collection container:  </strong>
                 A container with a capacity of at least 24-36 cups (6-9 liters) to hold the purified water.
               </li>
               <li>
-                <strong>Drinking cups:</strong>
+                <strong>Drinking cups:  </strong>
                 A cup for each person in the family, or a shared container with measurements marked for each person's daily ration.
               </li>
               <li>
-                <strong>Weight:</strong>
+                <strong>Weight:  </strong>
                 The solar still itself should not require any additional weight, as the water and plastic sheeting should provide enough weight to hold it in place.
               </li>
               <li>
-                <strong>Measurements:</strong>
+                <strong>Measurements:  </strong>
                 The size of the still should be large enough to provide adequate surface area for evaporation, but small enough to be manageable. A good rule of thumb is to aim for a still that is about 1-2 meters wide by 2-4 meters long.
               </li>
             </ol>
@@ -83,19 +83,19 @@ function WaterStill () {
             <p className="lead fs-4"><strong>Fresh Water Purification</strong></p>
             <ol>
               <li>
-                <strong>Boiling:</strong>
+                <strong>Boiling:  </strong>
                 One of the most straightforward methods of purifying water is to boil it for several minutes. This can be done using a fire or stove, and can effectively kill harmful bacteria and viruses.
               </li>
               <li>
-                <strong>SODIS (Solar Water Disinfection):</strong>
+                <strong>SODIS (Solar Water Disinfection):  </strong>
                 Similar to the solar still, this method uses sunlight to purify water. Water is placed in clear plastic bottles and exposed to direct sunlight for several hours, which can kill harmful microorganisms.
               </li>
               <li>
-                <strong>Filtration using sand and gravel:</strong>
+                <strong>Filtration using sand and gravel:  </strong>
                 In addition to using a makeshift filter with sand and charcoal, people in Gaza could also use layers of sand and gravel to filter out impurities from ocean water.
               </li>
               <li>
-                <strong>Precipitation:</strong>
+                <strong>Precipitation:  </strong>
                 If people have access to salt, they could add it to seawater to cause precipitation of suspended particles and microorganisms. The clear water on top could then be collected and boiled or filtered further.
               </li>
             </ol>
